92c95119b3f04343db16ee1f17d22bd8d74a54dd
Pavel Kartsel <pkartsel@gmail.com>
2019-06-20 21:02:03 +0200

    Update artificial caret after render

--- tracker.js
+++ tracker.js
@@ -45,6 +45,8 @@ const deleteSelection = (text, caret) => {
             }
             deleted = left - right - 1;
             break;
+        default:
+            throw Exception("Caret type is not supported");
 
     }
     deleted && shiftCaret(text, caret, deleted);
@@ -66,61 +68,63 @@ const clearSelection = (text, caret) => {
             for (let i = right; i >= left; i--) {
                 deleted = deleteChar(text, i);
             }
-            displayCaret(caret.end - 1);
+            displayCaret(text, caret.end - 1);
             break;
     }
     return deleted;
 };
-const displayCaret = (position) => {
-    console.log('setting caret to', position);
-    const el = document.getElementById('tracker');
-    const range = document.createRange();
-    const sel = window.getSelection();
-    let offset = 0;
-    const containerCount = el.childNodes.length; // todo filter olny chars
-    if (position >= containerCount) {
-        position = containerCount - 1;
-        offset = 1;
+const getCaretCoordinates = (caret, spans) => {
+    let caretPosition = caret.end;
+    let useRightBound = 0;
+    if (caretPosition === spans.length) {
+        caretPosition = spans.length - 1;
+        useRightBound = 1;
     }
-    const charNodes = Array.prototype.slice.call(el.childNodes).filter((n) => n.tagName === "SPAN");
-    const container = charNodes[position];
-    const textNode = container.childNodes[0];
-    range.setStart(textNode, offset);
-    range.collapse(true);
-    sel.removeAllRanges();
-    sel.addRange(range);
-    el.focus();
-};
-const getCaret = () => {
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-    const startNode = ["br", "#text"].indexOf(range.startContainer.nodeName.toLowerCase()) > -1
-        ? range.startContainer.parentElement : range.startContainer;
-    const endNode = ["br", "#text"].indexOf(range.endContainer.nodeName.toLowerCase()) > -1
-        ? range.endContainer.parentElement : range.endContainer;
-    const caret = {
-        type: selection.type,
-        rangeCount: selection.rangeCount,
-        start: +startNode.dataset['index'] + range.startOffset,
-        end: +endNode.dataset['index'] + range.endOffset
+    const target = spans[caretPosition];
+    const rect = target.getBoundingClientRect();
+    const x = rect.left + useRightBound * rect.width;
+    return {
+        x,
+        y: 0,
     };
-    return caret;
+};
+const getCaretFromClick = (mouseEvent, caret, charCount) => {
+    const target = mouseEvent.target;
+    if (!target.classList.contains("letter"))
+    {
+        return {
+            start: charCount,
+            end: charCount,
+            type: "Caret",
+        };
+    }
+
+    const rect = mouseEvent.target.getBoundingClientRect();
+    const letterX = mouseEvent.x - target.offsetLeft;
+    const closerToRight = Math.round(letterX / rect.width);
+    const targetIndex = Number(target.dataset["index"]);
+    const position = targetIndex + closerToRight;
+
+    return {
+        start: position,
+        end: position,
+        type: "Caret",
+    };
 };
 const shiftCaret = (text, caret, shift) => {
     let shifted = 0;
     if (shift) {
         const position = caret.end;
-        if (position + shift >= 0 && position + shift <= text.length) {
-            displayCaret(position + shift);
+        const newPosition = position + shift;
+        if (newPosition >= 0 && newPosition <= text.length) {
             shifted = shift;
         }
     }
-    console.log("shifted", shifted);
     caret.end += shifted;
     caret.start = caret.end;
     return shifted;
 };
-const processKey = (text, key, caret) => {
+const processKey = (text, key, caret, shiftQueue) => {
     clearSelection(text, caret);
     addChar(text, key, caret.end);
     shiftCaret(text, caret, 1);
@@ -135,15 +139,39 @@ const app = new Vue({
     data: {
         caret: {
             start: null,
-            end: null
+            end: null,
         },
-        formatted: [[codeName.normal, 'Hello, Vue.js '], [codeName.deleted, 'World'], [codeName.added, 'JS']],
+        caretUpdateRequired: false,
+        caretPosition: {
+            x: 0,
+            y: 0,
+        },
+        //formatted: [[codeName.normal, 'Hello, Vue.js '], [codeName.deleted, 'World'], [codeName.added, 'JS']],
+        formatted: [[codeName.normal, 'Hi']],
         decomposed: null
     },
-    computed: {},
+    updated: function () {
+        this.$nextTick(function () {
+            if (this.caretUpdateRequired) {
+                const spans = document.getElementsByClassName('letter');
+                console.log('caret should be shifted');
+                if (this.caret.end <= spans.length) {
+                    this.caretPosition = getCaretCoordinates(this.caret, spans);
+                    this.caretUpdateRequired = false;
+                } else {
+                    debugger;
+                }
+            }
+        })
+    },
     methods: {
-        updateCaret: function() {
-            this.caret = getCaret();
+        updateCaret: function(event) {
+            this.caret = getCaretFromClick(event, this.caret, this.decomposed.length);
+            const rendered = document.getElementsByClassName('letter');
+            this.caretPosition = getCaretCoordinates(this.caret, rendered)
+        },
+        doubleClick: function(event) {
+            console.log('double click', event);
         },
         input: function(event) {
             if (event && !event.metaKey) {
@@ -176,7 +204,14 @@ const app = new Vue({
                 break;
             }
 
-            this.updateCaret();
+            //update real caret position
+            const spans = document.getElementsByClassName('letter')
+            if (this.caret.end <= spans.length) {
+                this.caretPosition = getCaretCoordinates(this.caret, spans);
+            } else {
+                this.caretUpdateRequired = true; 
+                console.log('can not be set now, wait..')
+            }
         },
         paste: function(event) {
             event.preventDefault();
